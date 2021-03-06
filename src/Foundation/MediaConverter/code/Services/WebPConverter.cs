f68c548a79dbd1bced96bc7bede1499ce621ebee
Teemu Eronen <teemu.eronen@kraftvaerk.com>
2021-03-06 15:35:03 +0200

    ConvertToWebP static method and ImageProcessor library added. Reference to System.Drawing

--- src/Foundation/MediaConverter/code/Services/WebPConverter.cs
+++ src/Foundation/MediaConverter/code/Services/WebPConverter.cs
@@ -1,13 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Sitecore.Pipelines.HttpRequest;
+using System.IO;
+using ImageProcessor;
+using ImageProcessor.Plugins.WebP.Imaging.Formats;
 
 namespace SC2021KF.Foundation.MediaConverter.Services
 {
     public class WebPConverter
     {
-        
+        /// <summary>
+        /// Convert file to webp format
+        /// </summary>
+        /// <param name="fileBytes"></param>
+        /// <param name="filePath"></param>
+        public static void ConvertToWebP(byte[] fileBytes, string filePath)
+        {
+            try
+            {
+                using (Stream fs = new FileStream(filePath, FileMode.Create))
+                {
+                    using (ImageFactory imageFactory = new ImageFactory(preserveExifData: false))
+                    {
+                        imageFactory.Load(fileBytes)
+                                    .Format(new WebPFormat())
+                                    .Quality(100)
+                                    .Save(fs);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine(e.Message);
+            }
+        }
     }
 }
