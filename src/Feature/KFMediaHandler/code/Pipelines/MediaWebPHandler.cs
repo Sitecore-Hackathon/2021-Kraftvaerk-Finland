d5839eb8cba053934a6b660d6f06514aac2bcdc5
Niko Kukkonen <NIKU@kvk.kraftvaerk.com>
2021-03-06 17:48:52 +0200

    wip

--- src/Feature/KFMediaHandler/code/Pipelines/MediaWebPHandler.cs
+++ src/Feature/KFMediaHandler/code/Pipelines/MediaWebPHandler.cs
@@ -1,10 +1,15 @@
 ﻿using Sitecore.Data;
+using Sitecore.Data.Items;
 using Sitecore.Pipelines.Upload;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using Sitecore.Data.Items;
+using SC2021KF.Foundation.MediaConverter.Services;
+using System.IO;
+using Sitecore.Data.Fields;
+using System.Runtime.Serialization.Formatters.Binary;
+using Sitecore.Resources.Media;
 
 namespace SC2021KF.Feature.MediaHandler.Pipelines
 {
@@ -12,33 +17,48 @@ namespace SC2021KF.Feature.MediaHandler.Pipelines
     {
         public void Process(UploadArgs args)
         {
-            //Database db = Sitecore.Context.ContentDatabase;
-            var str = args.Parameters.Get("convertwebp");
-            if(str == "on")
+            Database db = Sitecore.Context.ContentDatabase;
+            Item webPMediaTemplate = db.GetItem("/sitecore/templates/jotain/jotain");
+
+            ID unversionedImage = new ID("{F1828A2C-7E5D-4BBD-98CA-320474871548}");
+            ID versionedImage = new ID("{C97BA923-8009-4858-BDD5-D8BE5FCCECF7}");
+            ID unversionedJpeg = new ID("{DAF085E8-602E-43A6-8299-038FF171349F}");
+            ID versionedPjeg = new ID("{EB3FB96C-D56B-4AC9-97F8-F07B24BB9BF7}");
+
+            ID webPBlobRow = new ID(""); //tämä pitää varmaan olla file-tyyppinen eikä attachment
+
+            foreach (Item mediaItem in args.UploadedItems)
             {
-                str = "yeah";
+                //jos kuva
+                if (mediaItem.TemplateID == unversionedImage || mediaItem.TemplateID == versionedImage || mediaItem.TemplateID == unversionedJpeg || mediaItem.TemplateID == versionedPjeg)
+                {
+                    //ImageField imageField = mediaItem.Fields["Image"];
+                    //MediaItem mediaItem2 = imageField.MediaItem;
+                    //Stream stream = mediaItem2.GetMediaStream();
+                    //Byte[] bytes = new Byte[stream.Length];
+                    //stream.Read(bytes, 0, bytes.Length);
+                    //img64.Src = "data:" + mediaItem2.MimeType + ";base64," + Convert.ToBase64String(bytes);
+
+                    byte[] webPMedia = null;
+                    BinaryFormatter bf = new BinaryFormatter();
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        bf.Serialize(ms, mediaItem.Fields["Blob"]);
+                        webPMedia = ms.ToArray();
+                    }
+
+
+                    //kutsutaan teemun koodia että saadaan webP muotoon itemi
+                    var webPMediaConverted = WebPConverter.ConvertToWebP(webPMedia);
+                    Stream stream = new MemoryStream(webPMediaConverted);
+                    var asfsdgrt = MediaManager.Creator.CreateFromStream(stream, mediaItem.Paths.Path, new MediaCreatorOptions());
+                    var asd = MediaManager.Creator.AttachStreamToMediaItem(stream, "mediaItemPath", "", null);
+                    //MediaItem mediaItem3 = asd;
+                    mediaItem.Editing.BeginEdit();
+                    //mediaItem["WebPFile"] = asfsdgrt;
+                    mediaItem.Editing.EndEdit();
+                }
             }
-            //Item webPMediaTemplate = db.GetItem("/sitecore/templates/jotain/jotain");
-
-            //ID unversionedImage = new ID("");
-            //ID versionedImage = new ID("");
-            //ID unversionedJpeg = new ID("");
-            //ID versionedPjeg = new ID("");
-
-            //ID webPBlobRow = new ID(""); //tämä pitää varmaan olla file-tyyppinen eikä attachment
-
-            //foreach (Item mediaItem in args.UploadedItems)
-            //{
-            //    //jos kuva
-            //    if (mediaItem.TemplateID == unversionedImage || mediaItem.TemplateID == versionedImage || mediaItem.TemplateID == unversionedJpeg || mediaItem.TemplateID == versionedPjeg)
-            //    {
-            //        //kutsutaan teemun koodia että saadaan webP muotoon itemi
-            //        //var webPMedia = funktio(mediaItem["Blob"]);
-            //        //mediaItem.Editing.BeginEdit();
-            //        //mediaItem["WebPFile"] = webPMedia;
-            //        //mediaItem.Editing.EndEdit();
-            //    }
-            //}
         }
     }
 }
