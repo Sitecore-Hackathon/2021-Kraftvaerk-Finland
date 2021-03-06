230644636354b8a292303a3f4f2329171774f158
Jon Koivula <jon.koivula@kraftvaerk.com>
2021-03-06 12:27:35 +0200

    Add two projects to solution

---
+++ src/Feature/WebPConverter/code/Pipelines/WebPConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Pipelines.HttpRequest;
+
+namespace SC2021KF.Feature.WebPConverter.Pipelines
+{
+    public class WebPConverter
+    {
+        
+    }
+}
---
+++ src/Feature/XRobotsTag/code/Pipelines/XRobotsTagHeaderAppender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Pipelines.HttpRequest;
+
+namespace SC2021KF.Feature.XRobotsTag.Pipelines
+{
+    public class XRobotsTagHeaderAppender : HttpRequestProcessor
+    {
+        public override void Process(HttpRequestArgs args)
+        {
+            Sitecore.Diagnostics.Log.Info("XRobotsTagHeaderAppender", typeof(XRobotsTagHeaderAppender));
+        }
+    }
+}
